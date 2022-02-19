2fbab5eaa39328781b0044843f2ccc09a1dd3ef9
Prince Singh <83390756+singhprinz@users.noreply.github.com>
2022-02-19 19:07:32 +0530

    Add files via upload

---
+++ java basic outputs/First.java
@@ -0,0 +1,8 @@
+class First
+{
+    public static void main(String args[])
+    {
+        System.out.println("United college  of engineering and research");
+        System.out.print("Advanced java trsaining");
+    }
+}
